b1575c57c4a9d4a3d6dd99383ea500bf6edc4b41
pipoop <pipoop@gmail.com>
2023-12-09 19:32:19 +0100

    hide github icon

--- app.py
+++ app.py
@@ -176,15 +176,12 @@ def main():
 
     # this markdown is for hiding "github" button
     st.markdown("<style>#MainMenu{visibility:hidden;}</style>", unsafe_allow_html=True)
-
+    st.markdown("<style>footer{visibility: hidden;}</style>", unsafe_allow_html=True)
+    st.markdown("<style>header{visibility: hidden;}</style>", unsafe_allow_html=True)
     st.markdown(
     """
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{display: none;} 
     </style>
     """,
     unsafe_allow_html=True
